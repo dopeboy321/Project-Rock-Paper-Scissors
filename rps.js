46e8bd7470a9ba25ccee073a3c70a4c87b431dc6
dopeboy321 <105025755+dopeboy321@users.noreply.github.com>
2023-02-27 16:26:22 +0100

    added prompt, function playAround

--- rps.js
+++ rps.js
@@ -4,4 +4,21 @@ function getComputerChoice () {
     return choice[random];
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  
+  if(playerSelection === "rock" || computerSelection === "paper") {
+    return "You win! Rock beats paper!";
+  } else if (playerSelection === "paper" || computerSelection === "rock") {
+    return "You lose! Rock beats paper!"
+  }
+  
+}
+
+let answer = prompt("Paper, Rock, Scissors?");
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
