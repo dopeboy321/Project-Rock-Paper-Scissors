c264da1c19164cd388152915c486a4ed842bd0d0
dopeboy321 <105025755+dopeboy321@users.noreply.github.com>
2023-02-27 14:32:25 +0100

    added function getComputerChoice

--- rps.js
+++ rps.js
@@ -1,1 +1,7 @@
-console.log("makumba")
+function getComputerChoice () {
+    let choice = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random() * 3);
+    return choice[random];
+}
+
+console.log(getComputerChoice());
