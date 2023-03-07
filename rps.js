62fecfc1abd3626d70c878e672292739ea164716
dopeboy321 <105025755+dopeboy321@users.noreply.github.com>
2023-03-07 13:34:05 +0100

    updated game function

--- rps.js
+++ rps.js
@@ -4,56 +4,55 @@ function getComputerChoice () {
     return choice[random];
 }
 
-
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
       return "You tie!";
   } else if(playerSelection === "rock" && computerSelection === "paper") {
+      computerScore++;
       return "You lose! Paper beats rock!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-      return "You win! Paper beats rock!!";
+      userScore++;
+      return "You win! Paper beats rock!";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      computerScore++;
       return "You lose! Rock beats scissors!";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      userScore++;
       return "You win! Rock beats scissors!";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      userScore++;
       return "You win! Scissors beats paper!";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      computerScore++;
       return "You lose! Scissors beats paper!";
   } else {
-    return "Something went wrong"
+    return "Something went wrong";
   }
 }
-
-
-let answer = prompt("Paper, Rock, Scissors?").toLowerCase();
-const playerSelection = answer;
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-
-// //Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-// Remember loops? This is a great opportunity to use one to play those five rounds:
-
-// for (let i = 0; i < 5; i++) {
-//    // your code here!
-// }
-// At this point you should be using console.log() to display the results of each round and the winner at the end.
-
 
 let userScore = 0;
 let computerScore = 0;
 
-function game(userScore, computerScore) {
-  
-  
-  for (let i = 0; i < 5; i++) {
-    if (userScore >= 5 && computerScore < 5) {
-      return "congratulation, you won!"
-    } else if (userScore < 5 && computerScore >= 5) {
-      return "that was close, try again"
-    }
-  }
-}
+      
+      function game() {
+        for (let i = 1; i <= 5; i++) {
+          let answer = prompt("Paper, Rock, Scissors?").toLowerCase();
+          let playerSelection = answer;
+          let computerSelection = getComputerChoice();
+          
+          playRound(playerSelection, computerSelection);
+              console.log(`Round: ${i}. The user score is ${userScore} and computer score is ${computerScore}`);
+        }
+        if (userScore > computerScore) {
+          alert("you won!");
+        } else if (computerScore > userScore) {
+          alert("you lost!");
+        } else {
+          alert("it's a draw!");
+        }
+      }
+      
+      game();
 
-console.log(game(userScore, computerScore));
+      
+      
