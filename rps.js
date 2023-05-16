1be293b732a7df948708e149a5ee62ac589798c6
dopeboy321 <105025755+dopeboy321@users.noreply.github.com>
2023-05-16 09:28:29 +0200

    added logic for picking the winner and manipulated DOM

--- rps.js
+++ rps.js
@@ -1,58 +1,73 @@
-function getComputerChoice () {
-    let choice = ["rock", "paper", "scissors"];
+function getComputerChoice() {
+    let choice = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * 3);
     return choice[random];
-}
+};
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-      return "You tie!";
-  } else if(playerSelection === "rock" && computerSelection === "paper") {
+      return 'You tie!';
+  } else if(playerSelection === 'rock' && computerSelection === 'paper') {
       computerScore++;
-      return "You lose! Paper beats rock!";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
+      return 'Paper beats rock!';
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
       userScore++;
-      return "You win! Paper beats rock!";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      return 'Paper beats rock!';
+  } else if (playerSelection === "scissors" && computerSelection === 'rock') {
       computerScore++;
-      return "You lose! Rock beats scissors!";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      return 'Rock beats scissors!';
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
       userScore++;
-      return "You win! Rock beats scissors!";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      return 'Rock beats scissors!';
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
       userScore++;
-      return "You win! Scissors beats paper!";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      return 'Scissors beats paper!';
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
       computerScore++;
-      return "You lose! Scissors beats paper!";
+      return 'Scissors beats paper!';
   } else {
-    return "Something went wrong";
-  }
-}
+    return 'Something went wrong';
+  }  
+};
 
+const choices = document.querySelectorAll('.choices');
 let userScore = 0;
 let computerScore = 0;
 
-      
-      function game() {
-        for (let i = 1; i <= 5; i++) {
-          let answer = prompt("Paper, Rock, Scissors?").toLowerCase();
-          let playerSelection = answer;
-          let computerSelection = getComputerChoice();
-          
-          playRound(playerSelection, computerSelection);
-              console.log(`Round: ${i}. The user score is ${userScore} and computer score is ${computerScore}`);
-        }
-        if (userScore > computerScore) {
-          alert("you won!");
-        } else if (computerScore > userScore) {
-          alert("you lost!");
-        } else {
-          alert("it's a draw!");
-        }
-      }
-      
-      game();
+choices.forEach(choice => choice.addEventListener('click', (event) => {
+  if(event.target.id == 'rock') {
+    results.textContent = playRound('rock', getComputerChoice());
+  } else if (event.target.id == 'paper') {
+    results.textContent = playRound('paper', getComputerChoice());
+  } else if (event.target.id == 'scissors') {
+    results.textContent = playRound('scissors', getComputerChoice());
+  };
+  updateScore();
+    if(userScore == 5) {
+      alert("You won =D");
+        restartScores();
+    } else if (computerScore == 5) {
+      alert("You lose :(");
+        restartScores();
+    };
+      }));
 
-      
-      
+function updateScore() {
+  const playerScorePara = document.getElementById('player-score');
+  const computerScorePara = document.getElementById('computer-score');
+
+  playerScorePara.textContent = `Player: ${userScore}`;
+  computerScorePara.textContent = `Computer: ${computerScore}`;
+};
+
+  function restartGame() {
+    restartScores();
+  };
+
+  function restartScores() {
+    userScore = 0;
+    computerScore = 0;
+    results.textContent = 'Start New Game!';
+    playerScorePara = userScore;
+    computerScorePara = computerScore;
+  };
